37da54dfd286ab08879f37fa04e50fcd80c464f8
viviane <vivianedasilvasouza2020@gmail.com>
2022-12-30 12:09:48 -0300

    feat: tabela categoria

--- database/migrations/2022_12_30_125741_create_categorias_table.php
+++ database/migrations/2022_12_30_125741_create_categorias_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -13,14 +14,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('nome', 150)->unique()->comment('Nome da categaoria');
+            $table->string('link', 250);
+            $table->string('ativo', 3);
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('categorias');
     }
 };
